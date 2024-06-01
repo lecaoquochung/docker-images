03fca09a677f01baa43eab7959c48c8f0f8a03b6
lecaoquochung <lecaoquochung@gmail.com>
2024-06-01 09:43:53 +0900

    jenkins -  dependencies

--- jenkins/dependencies.sh
+++ jenkins/dependencies.sh
@@ -20,4 +20,10 @@ apt-get update && \
   apt-get install -y git curl iputils-ping telnet vim unzip
 
 # Install mysql-client
-apt-get install -y default-mysql-client
+apt-get install -y default-mysql-client mariadb-client
+
+# Download AWS CLI 2.0 installer
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+aws --version
