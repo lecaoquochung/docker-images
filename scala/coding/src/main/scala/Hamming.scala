b52631a56d0d14a709646e89db96b2f1dbfeca97
lehungio <me@lehungio.com>
2024-10-17 15:09:42 +0000

    tweak

--- scala/coding/src/main/scala/Hamming.scala
+++ scala/coding/src/main/scala/Hamming.scala
@@ -1,8 +1,13 @@
 // https://rosalind.info/problems/hamm/
 object Hamming {
-  def distance(dnaStrandOne: String, dnaStrandTwo: String): Option[Int] = 
-    if (dnaStrandOne.length == dnaStrandTwo.length)
-      Some(dnaStrandOne zip dnaStrandTwo count { case (a, b) => a != b })
+  def distance(dnaStrandOne: String, dnaStrandTwo: String): Either[String, Int] = {
+    def isValidDNA(strand: String): Boolean = strand.forall(Set('A', 'C', 'G', 'T'))
+
+    if (!isValidDNA(dnaStrandOne) || !isValidDNA(dnaStrandTwo))
+      Left("Invalid DNA strand. DNA strands can only contain A, C, G, and T.")
+    else if (dnaStrandOne.length != dnaStrandTwo.length)
+      Left(s"DNA strands must be of equal length. Got ${dnaStrandOne.length} and ${dnaStrandTwo.length}.")
     else
-      None
+      Right(dnaStrandOne zip dnaStrandTwo count { case (a, b) => a != b })
+  }
 }
