4bb129968f4cdb93cf2920c0da4330e97039d3b2
lehungio <me@lehungio.com>
2024-10-17 15:14:55 +0000

    tweak

--- scala/coding/src/main/scala/Hamming.scala
+++ scala/coding/src/main/scala/Hamming.scala
@@ -1,13 +1,20 @@
 // https://rosalind.info/problems/hamm/
 object Hamming {
-  def distance(dnaStrandOne: String, dnaStrandTwo: String): Either[String, Int] = {
-    def isValidDNA(strand: String): Boolean = strand.forall(Set('A', 'C', 'G', 'T'))
-
-    if (!isValidDNA(dnaStrandOne) || !isValidDNA(dnaStrandTwo))
-      Left("Invalid DNA strand. DNA strands can only contain A, C, G, and T.")
-    else if (dnaStrandOne.length != dnaStrandTwo.length)
-      Left(s"DNA strands must be of equal length. Got ${dnaStrandOne.length} and ${dnaStrandTwo.length}.")
+  def distance(dnaStrandOne: String, dnaStrandTwo: String): Option[Int] = 
+    if (dnaStrandOne.length == dnaStrandTwo.length)
+      Some(dnaStrandOne zip dnaStrandTwo count { case (a, b) => a != b })
     else
-      Right(dnaStrandOne zip dnaStrandTwo count { case (a, b) => a != b })
-  }
+      None
 }
+
+// tweak
+// def distance(dnaStrandOne: String, dnaStrandTwo: String): Either[String, Int] = {
+//   def isValidDNA(strand: String): Boolean = strand.forall(Set('A', 'C', 'G', 'T'))
+
+//   if (!isValidDNA(dnaStrandOne) || !isValidDNA(dnaStrandTwo))
+//     Left("Invalid DNA strand. DNA strands can only contain A, C, G, and T.")
+//   else if (dnaStrandOne.length != dnaStrandTwo.length)
+//     Left(s"DNA strands must be of equal length. Got ${dnaStrandOne.length} and ${dnaStrandTwo.length}.")
+//   else
+//     Right(dnaStrandOne zip dnaStrandTwo count { case (a, b) => a != b })
+// }
