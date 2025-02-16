d85f140e78c1266e392f62dc43cae1264f4fb41b
lecaoquochung <lecaoquochung@gmail.com>
2025-02-16 22:58:01 +0900

    test:tweak

--- .circleci/os/linux/dependencies.sh
+++ .circleci/os/linux/dependencies.sh
@@ -10,24 +10,31 @@ google-chrome --version
 # yarn latest version
 # https://github.com/lecaoquochung/docker-images/issues/48
 # Check if corepack is available
-if ! command -v corepack &> /dev/null; then
-  echo "Installing corepack..."
-  sudo npm install -g corepack
-else
-  echo "Corepack is already installed"
-fi
+# if ! command -v corepack &> /dev/null; then # Check if corepack is not available -> Make sure this works on CircleCI
+#   echo "Installing corepack..."
+#   sudo npm install -g corepack
+# else
+#   echo "Corepack is already installed"
+# fi
+sudo npm install -g corepack
 corepack enable
 
-yarn upgrade; yarn install
+# Upgrade Yarn and install dependencies
+echo "Upgrading Yarn and installing dependencies..."
+yarn upgrade 
+yarn install
 yarn --version
 
+# Install global Yarn packages
+echo "Installing global Yarn packages..."
 yarn global add junit-report-merger
 yarn global bin
 
 export PATH="$PATH:/home/qa/.yarn/bin"
 
-# web driver
+# Install web driver
+echo "Installing Playwright..."
 npx playwright install
 
-# verify env var
+# Verify environment variable
 echo $PUPPETEER_EXECUTABLE_PATH
