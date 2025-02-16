697c2be4eb9e4fa45fb0b0eeb34fd180cf141b8c
me <me@server123lehungiocom.local>
2025-02-16 15:38:34 +0900

    prog:ts - test challenge 25

--- programming/typescript/challenges/index.js
+++ programming/typescript/challenges/index.js
@@ -4,8 +4,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Challenges = require("@lehungio/typescript");
 var helloAll = Challenges.greet('All'); // Output: "Hello, World!"
 console.log(helloAll);
+//Challenge 1: Hello, World!
 var typescript_1 = require("@lehungio/typescript");
 var greetMessage = (0, typescript_1.greet)('Typescript'); // Output: "Hello, World!"
 console.log(greetMessage);
 var helloWorld = new Challenges.HelloWorld();
 helloWorld.greet(); // Output: "Hello, World!"
+//Challenge 25: Word Count
+var typescript_2 = require("@lehungio/typescript");
+var count = (0, typescript_2.wordCount)('Hello, World!'); // Output: 2
+console.log(count);
--- programming/typescript/challenges/index.ts
+++ programming/typescript/challenges/index.ts
@@ -3,9 +3,14 @@ import * as Challenges from '@lehungio/typescript';
 let helloAll = Challenges.greet('All'); // Output: "Hello, World!"
 console.log(helloAll);
 
+//Challenge 1: Hello, World!
 import { greet } from '@lehungio/typescript';
 let greetMessage = greet('Typescript'); // Output: "Hello, World!"
 console.log(greetMessage);
-
 const helloWorld = new Challenges.HelloWorld();
 helloWorld.greet(); // Output: "Hello, World!"
+
+//Challenge 25: Word Count
+import { wordCount } from '@lehungio/typescript';
+const count = wordCount('Hello, World!'); // Output: 2
+console.log(count);
