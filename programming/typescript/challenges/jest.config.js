27ca585c53e16551580edc6740e997cb33114449
lecaoquochung <lecaoquochung@gmail.com>
2025-02-17 15:45:13 +0900

    test:tweak

--- programming/typescript/challenges/jest.config.js
+++ programming/typescript/challenges/jest.config.js
@@ -5,5 +5,18 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
   testMatch: ['**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // Add timeout setting
+  testTimeout: 5000,
+  // Add coverage thresholds
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  // Add setup files if needed
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 };
