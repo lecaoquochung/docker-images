a63d4edc7548100b901c054d7dc5eba6941f4cc1
lecaoquochung <lecaoquochung@gmail.com>
2025-03-25 21:14:17 +0900

    hello java

---
+++ programming/java/framework/spring/learn-sprint-framework/learn-sprint-framework/src/test/java/come/lehungio/learn_sprint_framework/LearnSprintFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package come.lehungio.learn_sprint_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSprintFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
